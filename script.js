0b9d198e398f683462a6e59fc9003e8361a2335e
Sarthak band <157968806+SarthakBand@users.noreply.github.com>
2025-03-15 20:26:01 +0530

    Updated JS

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let tb = document.querySelector("table")
+let data = localStorage.getItem("passwords")
+if (data == null){
+    tb.innerHTML = "No data to show"
+}
+else{
+     
+    let arr = JSON.parse(data);
+    let str = ""
+    for (let index =0;  index<arr.length; index++){
+        const element = arr[index];
+     str += `<tr>
+    <td>${element.website}</td>
+    <td>${element.username}</td>
+    <td>${element.password}</td>
+    <td>${"Delete"}</td>
+    </tr>`
+}
+    tb.innerHTML = tb.innerHTML + str;
+}
 console.log("Workiing")
 document.querySelector(".btn").addEventListener("click",(e)=>{
     e.preventDefault();
@@ -5,8 +25,15 @@ document.querySelector(".btn").addEventListener("click",(e)=>{
     console.log(username.value, password.value)
     let passwords = localStorage.getItem("passwords")
     console.log(passwords)
-    if(passwords){
+    if(passwords ==  null){
         let json = []
         json.push({username : username.value, password :  password.value})
+        alert("Password Saved")
+        localStorage.setItem("passwords", JSON.stringify(json))
+    } else{
+        let json = JSON.parse(localStorage.getItem("passwords"))
+        json.push({username : username.value, password :  password.value})
+        alert("Password Saved")
+        localStorage.setItem("passwords", JSON.stringify(json))
     }
 })
